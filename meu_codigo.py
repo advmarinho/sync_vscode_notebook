4dadd7e81f50770ecbeaf55a0e50a379232c3c11
Anderson Marinho <marinho.1983@gmail.com>
2022-03-19 16:42:50 -0300

    Update meu_codigo.py

--- meu_codigo.py
+++ meu_codigo.py
@@ -2,4 +2,4 @@ print('Esse é meu primeiro código em Python no Github!')
 
 print('Olá, mundo! escrevendo uma mensagem no meu primeiro código em Python no Github!')
 
-Linha editado no online
+# Linha editado no online
